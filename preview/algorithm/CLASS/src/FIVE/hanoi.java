af2af85ba5279744c81a58bb43093c53868660fe
LongLifeHwang <Rhig0617@gmail.com>
2024-04-18 14:28:47 +0900

    hanoi practice

--- preview/algorithm/CLASS/src/FIVE/hanoi.java
+++ preview/algorithm/CLASS/src/FIVE/hanoi.java
@@ -7,14 +7,15 @@ public class hanoi
     static void han(int N, int x, int y)
     {
         if (N > 1)
-            han(N - 1, x, 6 - x -y);
+            han(N - 1, x, 6 - x - y);
         System.out.printf("%d원반을 %d에서 %d로 옮김\n", N, x, y);
         if (N > 1)
             han(N - 1, 6 - x - y, y);
     }
+
     public static void  main(String[] args)
     {
-        int N;
+        int     N;
         Scanner stdIn = new Scanner(System.in);
 
         System.out.printf("N : ");
