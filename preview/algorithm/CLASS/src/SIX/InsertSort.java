b931996a3055c2a25333b5c51571cefa862a22aa
LongLifeHwang <Rhig0617@gmail.com>
2024-04-18 18:43:26 +0900

    insert practice

--- preview/algorithm/CLASS/src/SIX/InsertSort.java
+++ preview/algorithm/CLASS/src/SIX/InsertSort.java
@@ -6,27 +6,25 @@ public class InsertSort
 {
     static void insert(int[] x)
     {
+        int temp;
         int len;
-        int go;
         int j;
 
         len = x.length;
         for (int i = 1; i < len; i++)
         {
-            go = x[i];
-            for (j = i; j > 0 && x[j - 1] > go; j--)
-            {
+            temp = x[i];
+            for (j = i; j > 0 && x[j - 1] > temp; j--)
                 x[j] = x[j - 1];
-            }
-            x[j] = go;
+            x[j] = temp;
         }
     }
 
     public static void  main(String[] args)
     {
+        Scanner stdIn = new Scanner(System.in);
         int     N;
         int[]   x;
-        Scanner stdIn = new Scanner(System.in);
 
         System.out.printf("요솟 수 : ");
         N = stdIn.nextInt();
@@ -37,7 +35,7 @@ public class InsertSort
             x[i] = stdIn.nextInt();
         }
         insert(x);
-        System.out.printf("\n오름 차순으로 정렬하세요.\n");
+        System.out.printf("오름 차순으로 정렬했습니다. \n");
         for (int i = 0; i < N; i++)
         {
             System.out.printf("x[%d] : %d\n", i, x[i]);
