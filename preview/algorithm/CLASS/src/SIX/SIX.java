4fe0b2240d095a86066e2632c8e50c193f022312
LongLifeHwang <Rhig0617@gmail.com>
2024-04-18 16:15:07 +0900

    bubble sort2 practice

--- preview/algorithm/CLASS/src/SIX/SIX.java
+++ preview/algorithm/CLASS/src/SIX/SIX.java
@@ -2,15 +2,15 @@ package SIX;
 
 import java.util.Scanner;
 
-public class bubble_sort2
+public class SIX
 {
-    static void swap(int[] x, int n, int m)
+    static void swap(int[] x, int m, int n)
     {
         int temp;
 
-        temp = x[n];
-        x[n] = x[m];
-        x[m] = temp;
+        temp = x[m];
+        x[m] = x[n];
+        x[n] = temp;
     }
 
     static void sort2(int[] x)
@@ -19,20 +19,21 @@ public class bubble_sort2
         int k;
         int last;
 
-        k = 0;
         len = x.length;
+        k = 0;
+        last = 0;
         while (k < len - 1)
         {
-            last = len - 1;
-            for (int i = len - 1; i > k; i--)
+            k = len - 1;
+            for (int i = len - 1; i > last; i--)
             {
-                if (x[i] > x[i - 1])
+                if (x[i] < x[i - 1])
                 {
                     swap(x, i, i - 1);
-                    last = i;
+                    k = i;
                 }
             }
-            k = last;
+            last = k;
         }
     }
 
@@ -41,7 +42,8 @@ public class bubble_sort2
         int     N;
         int[]   x;
         Scanner stdIn = new Scanner(System.in);
-    
+
+        System.out.printf("요솟 수 : ");
         N = stdIn.nextInt();
         x = new int[N];
         for (int i = 0; i < N; i++)
@@ -50,7 +52,9 @@ public class bubble_sort2
             x[i] = stdIn.nextInt();
         }
         sort2(x);
+        System.out.printf("오름차순으로 정렬을 하였습니다. \n");
         for (int i = 0; i < N; i++)
             System.out.printf("x[%d] : %d\n", i, x[i]);
-    }    
+    }
 }
+
