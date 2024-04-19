d3ee84bb86a2367c206ecda423a23bf8d6a45aaa
LongLifeHwang <Rhig0617@gmail.com>
2024-04-19 22:11:25 +0900

    queue

---
+++ preview/algorithm/CLASS/src/FOUR/Queue.java
@@ -0,0 +1,63 @@
+package FOUR;
+
+public class Queue
+{
+    public class Empty extends RuntimeException
+    {
+        public Empty() {};
+    }
+
+    public class Over extends RuntimeException
+    {
+        public Over() {};
+    }
+
+    int     size;
+    int     idx;
+    int[]   x;
+
+    public Queue(int S)
+    {
+        S = size;
+        idx = 0;
+        x = new int[S];
+    }
+
+    public void inqueue(int n) throws Over
+    {
+        if (idx > size - 1)
+            throw new Over();
+        x[idx++] = n;
+    }
+
+    public void dequeue() throws Empty
+    {
+        if (idx == 0)
+            throw new Empty();
+        for (int i = 1; i < idx; i++)
+        {
+            x[i - 1] = x[i];
+        }
+    }
+
+    public int  getIdx()
+    {
+        return (idx);
+    }
+
+    public int  getSize()
+    {
+        return (size);
+    }
+
+    public void peek() throws Empty
+    {
+        if (idx == 0)
+            throw new Empty();
+        for (int i = 0; i < idx; i++)
+        {
+            System.out.printf("%d ", x[i]);
+        }
+        System.out.printf("\n");
+    }
+}
