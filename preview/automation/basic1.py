1292e101d576fb840668fbfc54fbd72e1501a37e
LongLifeHwang <Rhig0617@gmail.com>
2024-04-20 15:42:31 +0900

    print(f'test {0.987865 : .1f}')

--- preview/automation/basic1.py
+++ preview/automation/basic1.py
@@ -1,6 +1,12 @@
+PI = 3.14
+
+def cir_cirm(r = 5):
+    return (2 * r * PI)
+
 def squares(n):
     return (n * n)
 
 numbers = [1, 2, 3, 4]
 print(f'list : {list(map(squares, numbers))}')
+print(f'circle cirm : {cir_cirm(3):.2f}')
 
