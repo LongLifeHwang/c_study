aeaae4c0c922c25eb49df2929634792cfabf73b5
Ingyu Hwang <inghwang@c4r10s7.42seoul.kr>
2024-04-21 14:01:49 +0900

    del L[시작 인덱스 : 끝 인덱스의 바로 뒤 : 간격]

--- preview/automation/middle1.py
+++ preview/automation/middle1.py
@@ -1,3 +1,18 @@
-word = 'Dongyang Mirae Umiversity'
 print("====1====")
+word = 'Dongyang Mirae Umiversity'
 print(len(word))
+
+print("\n====2====")
+s = "Hi, everyon. Nice to meet you!"
+s = s.replace(',', '')
+s = s.replace('.', '')
+s = s.replace('!', '')
+print(s)
+
+print("\n====3====")
+L = [3, 6, 9]
+L.insert(1, 7)
+L.append(5)
+print(L)
+del L[:2] #마지막 인덱스는 체크하지 않음
+print(L)
