8187567822d2601f67dcb273d982dd1b1e7ff07e
LongLifeHwang <Rhig0617@gmail.com>
2024-04-22 20:53:21 +0900

    string

--- preview/automation/basic2.py
+++ preview/automation/basic2.py
@@ -1,8 +1,14 @@
 def ptrStr(str):
     print(f'{str}')
-
-
-
 str = "Hello world"
 ptrStr(str)
 
+test = {'a' : "hi", 'b' : 'bye'}
+for i in test.items():
+    print(i)
+    
+s = "Hi, everyone. Nice to meet you!"
+s = s.replace(",", "")
+s = s.replace('.', '')
+s = s.replace('!', '')
+print(s)
