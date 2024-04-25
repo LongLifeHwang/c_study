e909501fb787fc57d3f7e1f475949f745edd7340
LongLifeHwang <Rhig0617@gmail.com>
2024-04-25 21:25:13 +0900

    select sort

--- preview/algorithm/CLASS/src/test/selectSort.java
+++ preview/algorithm/CLASS/src/test/selectSort.java
@@ -4,9 +4,29 @@ import java.util.Scanner;
 
 public class selectSort
 {
+    static void swap(int[] x, int n, int m)
+    {
+        int temp;
+
+        temp = x[n];
+        x[n] = x[m];
+        x[m] = temp;
+    }
+
     static void sort(int[] x, int N)
     {
+        int origin;
 
+        for (int i = 0; i < N - 1; i++)
+        {
+            origin = i;
+            for (int j = i + 1; j < N; j++)
+            {
+                if (x[origin] > x[j])
+                    origin = j;
+            }
+            swap(x, i, origin);
+        }
     }
 
     public static void  main(String[] args)
